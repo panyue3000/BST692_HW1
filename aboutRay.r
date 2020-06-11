879ce470bfa71c2bf001729616eab3d7e5d872c6
Yue Pan <panyue3000@sina.com>
2020-06-11 16:19:23 -0400

    actual aboutRay.r

--- aboutRay.r
+++ aboutRay.r
@@ -1,2 +1,12 @@
 install.packages("roxygen2")
 library(roxygen2)
+
+#' @export aboutRay
+aboutRay <- function() {
+  cat("Which of these is a lie: \n")
+  cat("1. Ray's father was the sole survivor of an airplane crash.\n")
+  cat("2. Tom Cruise ran into Ray so hard at the Academy Award they both spilled their drinks.\n")
+  cat("3. Ray's niece was Billie Eilish's best friend as they grew up.\n")
+  cat("4. Ray's father-in-law was accused of covering up the murder of Marilyn Monroe.\n")
+  cat("5. Ray took his son to the world premiere of the SpongeBob SquarePants movie in 2004.\n")
+}
